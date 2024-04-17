0271be7dd26be88d5e1482660431e067904ba755
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-04-17 22:55:39 +0630

    feat: implement custom dragging preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { AddNewItem } from "./AddNewItem";
 import { Column } from "./Column";
+import { CustomDragLayer } from "./CustomDragLayer";
 import { useAppState } from "./state/AppStateContext";
 import { addList } from "./state/actions";
 import { AppContainer } from "./styles";
@@ -9,10 +10,10 @@ export const App = () => {
 
   return (
     <AppContainer>
+      <CustomDragLayer />
       {lists.map((list) => (
         <Column id={list.id} text={list.text} />
       ))}
-
       <AddNewItem
         toggleButtonText="+ Add another list"
         onAdd={(text) => dispatch(addList(text))}
