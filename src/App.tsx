d4eaca707f79747e60fd1aadbbf831b4cedd9578
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-04-17 19:32:45 +0630

    feat: getting columns by using context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { AddNewItem } from "./AddNewItem";
 import { Column } from "./Column";
+import { useAppState } from "./state/AppStateContext";
 import { AppContainer } from "./styles";
 
 export const App = () => {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="Todo:" />
+      {lists.map((list) => (
+        <Column id={list.id} text={list.text} />
+      ))}
+
       <AddNewItem
         toggleButtonText="+ Add another list"
         onAdd={() => console.log("Item created")}
