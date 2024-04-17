1b844d0e0a737b31cd7ddd297ad0dda8bed82392
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-04-17 19:15:49 +0630

    feat: getting tasks from context by useAppState hook

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,18 +1,25 @@
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
+import { useAppState } from "./state/AppStateContext";
 import { ColumnContainer, ColumnTitle } from "./styles";
 
 type ColumnProps = {
+  id: string;
   text: string;
 };
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column = ({ id, text }: ColumnProps) => {
+  const { getTasksByListId } = useAppState();
+
+  const tasks = getTasksByListId(id);
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <Card text="Generate App Scaffold" />
-      <Card text="Learn Typescript" />
-      <Card text="Begin to Use Static Typing" />
+      {tasks.map((task) => (
+        <Card key={task.id} id={task.id} text={task.text} />
+      ))}
+
       <AddNewItem
         toggleButtonText="+ Add another card"
         onAdd={() => console.log("New Item added")}
