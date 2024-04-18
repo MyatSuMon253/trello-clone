5b42df5f8b731da688d235707374d9af46613c27
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-04-18 21:50:10 +0630

    feat: drag card to empty column

--- src/Column.tsx
+++ src/Column.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 import { useAppState } from "./state/AppStateContext";
-import { addTask, moveList } from "./state/actions";
+import { addTask, moveList, moveTask, setDraggedItem } from "./state/actions";
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { useItemDrag } from "./utils/useItemDrag";
 import { useDrop } from "react-dnd";
@@ -21,7 +21,7 @@ export const Column = ({ id, text, isPreview }: ColumnProps) => {
   const ref = useRef<HTMLDivElement>(null);
   const { drag } = useItemDrag({ type: "COLUMN", id, text });
   const [, drop] = useDrop({
-    accept: "COLUMN",
+    accept: ["COLUMN", "CARD"],
     hover: throttle(200, () => {
       if (!draggedItem) {
         return;
@@ -31,6 +31,15 @@ export const Column = ({ id, text, isPreview }: ColumnProps) => {
           return;
         }
         dispatch(moveList(draggedItem.id, id));
+      } else {
+        if (draggedItem.columnId === id) {
+          return;
+        }
+        if (tasks.length) {
+          return;
+        }
+        dispatch(moveTask(draggedItem.id, null, draggedItem.columnId, id));
+        dispatch(setDraggedItem({ ...draggedItem, columnId: id }));
       }
     }),
   });
